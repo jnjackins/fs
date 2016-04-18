108d53c9a2ba2c2f1cdcf58c066e4627510d74d6
Jeremy Jackins <jeremyjackins@gmail.com>
2016-04-18 12:59:27 +0900

    fossil: fix ioctl usage on linux, again

--- fossil/compat_linux.go
+++ fossil/compat_linux.go
@@ -3,14 +3,15 @@ package main
 // #include <linux/hdreg.h>
 // #include <linux/fs.h>
 import "C"
+import "unsafe"
 
 func devsize(fd uintptr) int64 {
 	var u64 uint64
-	if err := ioctl(fd, C.BLKGETSIZE64, &u64); err == nil {
+	if err := ioctl(fd, C.BLKGETSIZE64, uintptr(unsafe.Pointer(&u64))); err == nil {
 		return int64(u64)
 	}
 	var l int
-	if err := ioctl(fd, C.BLKGETSIZE, &l); err == nil {
+	if err := ioctl(fd, C.BLKGETSIZE, uintptr(unsafe.Pointer(&l))); err == nil {
 		return int64(l * 512)
 	}
 	return 0
