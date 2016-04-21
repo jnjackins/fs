2eb891ebf605d43c0fb832b46b85a3bb7d89a7a6
Jeremy Jackins <jeremyjackins@gmail.com>
2016-04-21 18:21:30 +0900

    fossil: remove cgo requirement

--- fossil/compat_darwin.go
+++ fossil/compat_darwin.go
@@ -1,15 +1,17 @@
 package main
 
-// #include <sys/disk.h>
-import "C"
-
 import "unsafe"
+
+const (
+	_DKIOCGETBLOCKSIZE  = 0x40046418
+	_DKIOCGETBLOCKCOUNT = 0x40086419
+)
 
 func devsize(fd uintptr) int64 {
 	var bs uint64
 	var bc uint64
-	ioctl(fd, C.DKIOCGETBLOCKSIZE, uintptr(unsafe.Pointer(&bs)))
-	ioctl(fd, C.DKIOCGETBLOCKCOUNT, uintptr(unsafe.Pointer(&bc)))
+	ioctl(fd, _DKIOCGETBLOCKSIZE, uintptr(unsafe.Pointer(&bs)))
+	ioctl(fd, _DKIOCGETBLOCKCOUNT, uintptr(unsafe.Pointer(&bc)))
 	if bs > 0 && bc > 0 {
 		return int64(bc * bs)
 	}
--- fossil/compat_linux.go
+++ fossil/compat_linux.go
@@ -1,17 +1,19 @@
 package main
 
-// #include <linux/hdreg.h>
-// #include <linux/fs.h>
-import "C"
 import "unsafe"
+
+const (
+	_BLKGETSIZE64 = 0x80081272
+	_BLKGETSIZE   = 0x1260
+)
 
 func devsize(fd uintptr) int64 {
 	var u64 uint64
-	if err := ioctl(fd, C.BLKGETSIZE64, uintptr(unsafe.Pointer(&u64))); err == nil {
+	if err := ioctl(fd, _BLKGETSIZE64, uintptr(unsafe.Pointer(&u64))); err == nil {
 		return int64(u64)
 	}
 	var l int
-	if err := ioctl(fd, C.BLKGETSIZE, uintptr(unsafe.Pointer(&l))); err == nil {
+	if err := ioctl(fd, _BLKGETSIZE, uintptr(unsafe.Pointer(&l))); err == nil {
 		return int64(l * 512)
 	}
 	return 0
